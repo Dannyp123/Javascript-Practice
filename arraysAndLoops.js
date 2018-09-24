bc18cbb9e35e863df71727dfc3b22f8f9b96529d
Daniel <dpeterson@basecampcodingacademy.org>
2018-09-24 15:35:12 -0500

    finished arrays and loops

--- arraysAndLoops.js
+++ arraysAndLoops.js
@@ -1,22 +1,53 @@
 // (Array) -> Array
 // Returns an array whose members are the last three members of the
 // input array. If the array has less than 3 members returns the array.
-function lastThree(arr) {}
+function lastThree(arr) {
+    if (arr.length < 3) {
+        return arr;
+    } else {
+        return [arr[arr.length - 3], arr[arr.length - 2], arr[arr.length - 1]];
+    }
+}
 
 // (Array[Number]) -> Number
 // Returns the sum of all members of the array.
-function sum(arr) {}
+function sum(arr) {
+    var total = 0;
+    for (var i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
 
 // (Array[Number], Array[Number]) -> Number
 // Returns the sum of the longer Array.
 // If both Arrays are the same length, returns the sum of both Arrays.
 // HINT: Use the sum function you wrote above.
-function sumOfLonger(arr1, arr2) {}
+function sumOfLonger(arr1, arr2) {
+    if (arr1.length > arr2.length) {
+        return sum(arr1);
+    } else if (arr1.length < arr2.length) {
+        return sum(arr2);
+    } else {
+        return sum(arr1) + sum(arr2);
+    }
+}
 
 // (Array, Array) -> Bool
 // Returns true if the two arrays are identical (contain the same elements
 // in the same order).
-function arrayEquals(arr1, arr2) {}
+function arrayEquals(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 // (Array[Number]) -> Array[Number]
 // Returns the difference between each cost and the minimum cost.
@@ -24,17 +55,47 @@ function arrayEquals(arr1, arr2) {}
 // For example, if our options were [1, 3, 3, 2, 5].
 // The minimum cost would be 1, and the differences from that
 // minimum would be [0, 2, 2, 1, 4]
-function differenceFromMinimum(costs) {}
+function differenceFromMinimum(costs) {
+    var min = costs[1];
+    for (var i = 0; i < costs.length; i++) {
+        if (costs[i] < min) {
+            min = costs[x];
+        }
+    }
+    var newCosts = [];
+    for (var i = 0; i < costs.length; i++) {
+        newCosts.push(costs[i] - min);
+    }
+    return newCosts;
+}
 
 // (String) -> Array[String]
 // Returns the hashtags used in the provided tweet.
-function hashtags(tweet) {}
+function hashtags(tweet) {
+    var tags = [];
+    var hashtag = tweet.split(" ");
+    for (var x = 0; x < hashtag.length; x++) {
+        if (hashtag[x].startsWith("#")) {
+            tags.push(hashtag[x]);
+        }
+    }
+    return tags;
+}
 
 // (String) -> Array[String]
 // Returns the mentions used in the provided tweet.
 // HINT: Can you make a utility function that works for both hashtags
 // and mentions?
-function mentions(tweet) {}
+function mentions(tweet) {
+    var tags = [];
+    var hashtag = tweet.split(" ");
+    for (var x = 0; x < hashtag.length; x++) {
+        if (hashtag[x].startsWith("@")) {
+            tags.push(hashtag[x]);
+        }
+    }
+    return tags;
+}
 
 // (String) -> Array[String, Float, Float]
 // Return an inventory item encoded in the provided string.
@@ -43,7 +104,18 @@ function mentions(tweet) {}
 //     - price we paid
 //     - price we're charging
 // The provided string seperates each of these pieces with a tab.
-function parseInventoryString(invStr) {}
+function parseInventoryString(invStr) {
+    var inventory = [];
+    var parse = invStr.split("\t");
+    for (var words = 0; words < parse.length; words++) {
+        if (isNaN(parse[words]) === true) {
+            inventory.push(parse[words]);
+        } else {
+            inventory.push(Number(parse[words]));
+        }
+    }
+    return inventory;
+}
 
 exports.lastThree = lastThree;
 exports.sum = sum;
--- arraysAndLoops.test.js
+++ arraysAndLoops.test.js
@@ -1,79 +1,79 @@
-var ll = require('./arraysAndLoops.js');
+var ll = require("./arraysAndLoops.js");
 
-describe('lastThree', function() {
-    test('lastThree of [1, 2, 3] should be [1, 2, 3]', function() {
+describe("lastThree", function() {
+    test("lastThree of [1, 2, 3] should be [1, 2, 3]", function() {
         expect(ll.lastThree([1, 2, 3])).toEqual([1, 2, 3]);
     });
 
-    test('lastThree of [1, 2, 3, 4] should be [2, 3, 4]', function() {
+    test("lastThree of [1, 2, 3, 4] should be [2, 3, 4]", function() {
         expect(ll.lastThree([1, 2, 3, 4])).toEqual([2, 3, 4]);
     });
 
-    test('lastThree of [] should be []', function() {
+    test("lastThree of [] should be []", function() {
         expect(ll.lastThree([])).toEqual([]);
     });
 
-    test('lastThree of [1, 2] should be [1, 2]', function() {
+    test("lastThree of [1, 2] should be [1, 2]", function() {
         expect(ll.lastThree([1, 2])).toEqual([1, 2]);
     });
 
-    test('lastThree of [5, 4, 3, 2] should be [4, 3, 2]', function() {
+    test("lastThree of [5, 4, 3, 2] should be [4, 3, 2]", function() {
         expect(ll.lastThree([5, 4, 3, 2])).toEqual([4, 3, 2]);
     });
 });
 
-describe('sum', function() {
-    test('sum of [1, 2, 3] should be 6', function() {
+describe("sum", function() {
+    test("sum of [1, 2, 3] should be 6", function() {
         expect(ll.sum([1, 2, 3])).toEqual(6);
     });
 
-    test('sum of [] should be 0', function() {
+    test("sum of [] should be 0", function() {
         expect(ll.sum([])).toEqual(0);
     });
 });
 
-describe('sumOfLonger', function() {
-    test('sumOfLonger between [] and [1] should be 1', function() {
+describe("sumOfLonger", function() {
+    test("sumOfLonger between [] and [1] should be 1", function() {
         expect(ll.sumOfLonger([], [1])).toEqual(1);
     });
 
-    test('sumOfLonger between [1] and [1] should be 2', function() {
+    test("sumOfLonger between [1] and [1] should be 2", function() {
         expect(ll.sumOfLonger([1], [1])).toEqual(2);
     });
 
-    test('sumOfLonger between [2] and [1] should be 3', function() {
+    test("sumOfLonger between [2] and [1] should be 3", function() {
         expect(ll.sumOfLonger([2], [1])).toEqual(3);
     });
 
-    test('sumOfLonger between [2, 2] and [1] should be 4', function() {
+    test("sumOfLonger between [2, 2] and [1] should be 4", function() {
         expect(ll.sumOfLonger([2, 2], [1])).toEqual(4);
     });
 
-    test('sumOfLonger between [1, 2, 3] and [1] should be 6', function() {
+    test("sumOfLonger between [1, 2, 3] and [1] should be 6", function() {
         expect(ll.sumOfLonger([1, 2, 3], [1])).toEqual(6);
     });
 
-    test('sumOfLonger between [1, 2, 3], [1, 2, 3 and 4] should be 10', function() {
+    test("sumOfLonger between [1, 2, 3], [1, 2, 3 and 4] should be 10", function() {
         expect(ll.sumOfLonger([1, 2, 3], [1, 2, 3, 4])).toEqual(10);
     });
 });
 
-describe('arrayEquals', function() {
-    test('[1, 2, 3] equals [1, 2, 3]', function() {
-      expect(objects.arrayEquals([1, 2, 3], [1, 2, 3])).toEqual(true);
+describe("arrayEquals", function() {
+    test("[1, 2, 3] equals [1, 2, 3]", function() {
+        expect(ll.arrayEquals([1, 2, 3], [1, 2, 3])).toEqual(true);
     });
 
-    test('[] equals []', function() {
-      expect(objects.arrayEquals([], [])).toEqual(true);
+    test("[] equals []", function() {
+        expect(ll.arrayEquals([], [])).toEqual(true);
     });
 
-    test('[1] does not equal [1, 2]', function() {
-      expect(objects.arrayEquals([1], [1, 2])).toEqual(false);
+    test("[1] does not equal [1, 2]", function() {
+        expect(ll.arrayEquals([1], [1, 2])).toEqual(false);
     });
 });
 
-describe('differenceFromMinimum', function() {
-    test('differenceFromMinimum for [260.0, 195.0, 390.0] should be [65.0, 0.0, 195.0]', function() {
+describe("differenceFromMinimum", function() {
+    test("differenceFromMinimum for [260.0, 195.0, 390.0] should be [65.0, 0.0, 195.0]", function() {
         expect(ll.differenceFromMinimum([260.0, 195.0, 390.0])).toEqual([
             65.0,
             0.0,
@@ -81,7 +81,7 @@ describe('differenceFromMinimum', function() {
         ]);
     });
 
-    test('differenceFromMinimum for [650.0, 65.0, 2080.0] should be [585.0, 0.0, 2015.0]', function() {
+    test("differenceFromMinimum for [650.0, 65.0, 2080.0] should be [585.0, 0.0, 2015.0]", function() {
         expect(ll.differenceFromMinimum([650.0, 65.0, 2080.0])).toEqual([
             585.0,
             0.0,
@@ -90,65 +90,65 @@ describe('differenceFromMinimum', function() {
     });
 });
 
-describe('hashtags', function() {
+describe("hashtags", function() {
     test("hashtags('#python is #totes the best.') should be ['#python', '#totes']", function() {
-        expect(ll.hashtags('#python is #totes the best.')).toEqual([
-            '#python',
-            '#totes'
+        expect(ll.hashtags("#python is #totes the best.")).toEqual([
+            "#python",
+            "#totes"
         ]);
     });
 
     test("hashtags('@nate ... who says #totes') should be ['#totes']", function() {
-        expect(ll.hashtags('@nate ... who says #totes')).toEqual(['#totes']);
+        expect(ll.hashtags("@nate ... who says #totes")).toEqual(["#totes"]);
     });
 
     test("hashtags('@SomeFashionStore is having a #FireSale on trendy #Totes') should be ['#FireSale', '#Totes']", function() {
         expect(
             ll.hashtags(
-                '@SomeFashionStore is having a #FireSale on trendy #Totes'
+                "@SomeFashionStore is having a #FireSale on trendy #Totes"
             )
-        ).toEqual(['#FireSale', '#Totes']);
+        ).toEqual(["#FireSale", "#Totes"]);
     });
 });
 
-describe('mentions', function() {
+describe("mentions", function() {
     test("mentions('#python is #totes the best.') should be []", function() {
-        expect(ll.mentions('#python is #totes the best.')).toEqual([]);
+        expect(ll.mentions("#python is #totes the best.")).toEqual([]);
     });
 
     test("mentions('@nate ... who says #totes') should be ['@nate']", function() {
-        expect(ll.mentions('@nate ... who says #totes')).toEqual(['@nate']);
+        expect(ll.mentions("@nate ... who says #totes")).toEqual(["@nate"]);
     });
 
     test("mentions('@SomeFashionStore is having a #FireSale on trendy #Totes') should be ['@SomeFashionStore']", function() {
         expect(
             ll.mentions(
-                '@SomeFashionStore is having a #FireSale on trendy #Totes'
+                "@SomeFashionStore is having a #FireSale on trendy #Totes"
             )
-        ).toEqual(['@SomeFashionStore']);
+        ).toEqual(["@SomeFashionStore"]);
     });
 });
 
-describe('parseInventoryString', function() {
-    test("parseInventoryString('Coke\\t0.40\\t1.00') should be ['Coke', 0.4, 1.0]", function() {
-        expect(ll.parseInventoryString('Coke\t0.40\t1.00')).toEqual([
-            'Coke',
+describe("parseInventoryString", function() {
+    test("parseInventoryString('Coke\t0.40\t1.00') should be ['Coke', 0.4, 1.0]", function() {
+        expect(ll.parseInventoryString("Coke\t0.40\t1.00")).toEqual([
+            "Coke",
             0.4,
             1.0
         ]);
     });
 
-    test("parseInventoryString('Tab\\t0.54\\t1.49') should be ['Tab', 0.54, 1.49]", function() {
-        expect(ll.parseInventoryString('Tab\t0.54\t1.49')).toEqual([
-            'Tab',
+    test("parseInventoryString('Tab\t0.54\t1.49') should be ['Tab', 0.54, 1.49]", function() {
+        expect(ll.parseInventoryString("Tab\t0.54\t1.49")).toEqual([
+            "Tab",
             0.54,
             1.49
         ]);
     });
 
-    test("parseInventoryString('Pepsi\\t1\\t1.49') should be ['Pepsi', 1.0, 1.49]", function() {
-        expect(ll.parseInventoryString('Pepsi\t1\t1.49')).toEqual([
-            'Pepsi',
+    test("parseInventoryString('Pepsi\t1\t1.49') should be ['Pepsi', 1.0, 1.49]", function() {
+        expect(ll.parseInventoryString("Pepsi\t1\t1.49")).toEqual([
+            "Pepsi",
             1.0,
             1.49
         ]);
