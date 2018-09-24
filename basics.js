60235d912ee3358428b00e828f3a6f7c1803dc06
Daniel <dpeterson@basecampcodingacademy.org>
2018-09-24 11:20:48 -0500

    first JS commit

--- basics.js
+++ basics.js
@@ -1,14 +1,20 @@
 // (Number, Number) -> Number
 // Returns the sum of a and b.
-function add(a, b) {}
+function add(a, b) {
+    return a + b;
+}
 
 // (Number) -> Number
 // Returns x multiplied by 2.
-function double(x) {}
+function double(x) {
+    return x * 2;
+}
 
 // (Number, Number, Number) -> Number
 // Returns the product of x, y, and z.
-function multThree(x, y, z) {}
+function multThree(x, y, z) {
+    return x * y * z;
+}
 
 // (Int, Int, Int) -> Int
 // Returns the total earnings provided the number of A, B, and C class seats sold.
@@ -16,23 +22,39 @@ function multThree(x, y, z) {}
 //     Class A seats cost $15.
 //     Class B seats cost $12.
 //     Class C seats cost $9.
-function earnings(a, b, c) {}
+function earnings(a, b, c) {
+    return a * 15 + b * 12 + c * 9;
+}
 
 // (Bool, Bool) -> Bool
 // Returns true if both x and y are true.
-function both(x, y) {}
+function both(x, y) {
+    return x && y;
+}
 
 // (Int, Bool) -> String
 // Returns 'walk' if the user should walk
 // and 'drive' if the user should drive to their destination.
 // The user should walk if it is nice weather
 // and the distance is a quarter mile or less.
-function walkOrDrive(miles, isNiceWeather) {}
+function walkOrDrive(miles, isNiceWeather) {
+    if (miles <= 0.25 && isNiceWeather) {
+        return "walk";
+    } else {
+        return "drive";
+    }
+}
 
 // (Int, Number) -> String
 // Return 'Densely Populated' if the land density is greater than 100,
-// otherwise retuurn 'Sparsely Populated'.
-function howPopulated(population, landArea) {}
+// otherwise return 'Sparsely Populated'.
+function howPopulated(population, landArea) {
+    if (population / landArea > 100) {
+        return "Densely Populated";
+    } else {
+        return "Sparsely Populated";
+    }
+}
 
 // (Int) -> String
 // Return the appropriate number of stars for the users score.
@@ -42,7 +64,19 @@ function howPopulated(population, landArea) {}
 // 3 stars for scores less than 8000 points
 // 4 stars for scores less than 10000 points
 // 5 stars for anything 10000+
-function goldStars(points) {}
+function goldStars(points) {
+    if (points < 1000) {
+        return "*";
+    } else if (points < 5000) {
+        return "**";
+    } else if (points < 8000) {
+        return "***";
+    } else if (points < 10000) {
+        return "****";
+    } else if (points >= 10000) {
+        return "*****";
+    }
+}
 
 // (String) -> Int
 // Returns the number of points for the corresponding
@@ -52,7 +86,19 @@ function goldStars(points) {}
 // A safety is worth 2 points.
 // A field goal is worth 3 points.
 // A touchdown is worth 6 points.
-function howManyPoints(scoringAction) {}
+function howManyPoints(scoringAction) {
+    if (scoringAction === "extra kick") {
+        return 1;
+    } else if (scoringAction === "extra conv") {
+        return 2;
+    } else if (scoringAction === "safety") {
+        return 2;
+    } else if (scoringAction === "fg") {
+        return 3;
+    } else if (scoringAction === "td") {
+        return 6;
+    }
+}
 
 exports.add = add;
 exports.double = double;
